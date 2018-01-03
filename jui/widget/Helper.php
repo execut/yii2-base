20f8b14a8e1949938f125dd1bbe83742c87bfa0c
Mehmet Şeref Kayalı <mskayali@yandex.com>
2018-01-03 10:36:03 +0300

    BaseObject class error fixed. It's urgent..

--- jui/widget/Helper.php
+++ jui/widget/Helper.php
@@ -9,10 +9,10 @@
 namespace execut\yii\jui\widget;
 
 
-use yii\base\BaseObject;
+use yii\base\Object;
 use yii\helpers\Inflector;
 
-class Helper extends BaseObject
+class Helper extends Object
 {
     public $widget = null;
     public function getBundleClass() {
