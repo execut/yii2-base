e3634df539b94abb6751581c005a449e769845fa
eXeCUT <sexecut@gmail.com>
2019-04-30 15:25:58 +0300

    Added input widget

---
+++ jui/InputWidget.php
@@ -0,0 +1,9 @@
+<?php
+namespace execut\yii\jui;
+use execut\yii\jui\widget\Renderer;
+use yii\helpers\Html;
+use yii\helpers\Inflector;
+
+class InputWidget extends \yii\jui\InputWidget {
+    use WidgetTrait;
+} 
